14badf47b2645805b010c3f7bf74037c38c1041a
Jared Sy <jaredblase@gmail.com>
2023-08-19 18:39:23 +0800

    add: send email function

---
+++ app/contact-us/api/send-email.ts
@@ -0,0 +1,16 @@
+import nodemailer from 'nodemailer'
+
+export async function sendEmail(to: string, subject: string, html: string, cc?: string) {
+	const user = process.env.MAIL_USER
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user,
+			pass: process.env.MAIL_PASS,
+		},
+		tls: {
+			rejectUnauthorized: false
+		}
+	})
+	return await transporter.sendMail({ from: `CitiWorld <${user}>`, to, subject, html, cc });
+}
