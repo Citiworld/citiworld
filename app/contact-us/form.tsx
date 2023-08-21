55534a4887d76f81f9e5b8ec098928fbb2cdc9f5
Jared Sy <jaredblase@gmail.com>
2023-08-21 20:50:14 +0800

    add: loading button to contact form

--- app/contact-us/form.tsx
+++ app/contact-us/form.tsx
@@ -1,33 +1,40 @@
 'use client'
 import { FormEventHandler, useState } from 'react'
 import styles from './contact-us.module.css'
+import { LoadingButton } from '@/components/loading-button'
 
 export function ContactForm() {
 	const [error, setError] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
 
 	const handleSubmit: FormEventHandler = async e => {
 		e.preventDefault()
-		const form = e.target as HTMLFormElement
+		setIsLoading(true)
+		try {
+			const form = e.target as HTMLFormElement
 
-		const data = new FormData(form)
-		const name = data.get('name')
-		const email = data.get('email')
-		const subject = data.get('subject')
-		const body = data.get('body')
+			const data = new FormData(form)
+			const name = data.get('name')
+			const email = data.get('email')
+			const subject = data.get('subject')
+			const body = data.get('body')
+	
+			if (!name || !email || !subject || !body) return setError('Missing field values.')
+	
+			const res = await fetch('/contact-us/api', {
+				method: 'POST',
+				body: data
+			})
 
-		if (!name || !email || !subject || !body) return setError('Missing field values.')
-
-		const res = await fetch('/contact-us/api', {
-			method: 'POST',
-			body: data
-		})
-
-		if (!res.ok) return setError('An error has occurred. Please try again.')
-
-		const { error } = await res.json()
-		if (error) return setError(error)
-		form.reset()
-		setError(error ?? "Your query has been sent.")
+			if (!res.ok) return setError('An error has occurred. Please try again.')
+	
+			const { error } = await res.json()
+			if (error) return setError(error)
+			form.reset()
+			setError(error ?? "Your query has been sent.")
+		} finally {
+			setTimeout(() => setIsLoading(false), 3000)
+		}
 	}
 
 	const clearError = () => setError('')
@@ -52,7 +59,9 @@ export function ContactForm() {
 				<textarea required onChange={clearError} name="body" id="body" rows={6} />
 			</div>
 			<p className="text-red-500 h-6">{error}</p>
-			<input type="submit" value="Send" className="btn highlight w-full" />
+			<LoadingButton type="submit" className="btn highlight w-full" isLoading={isLoading}>
+				Send
+			</LoadingButton>
 		</form>
 	)
 }
