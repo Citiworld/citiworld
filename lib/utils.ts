9ec3e9af79b3f86ec4e81cf7bad3870f8a1ea3b6
Jared Sy <jaredblase@gmail.com>
2023-09-05 21:10:44 +0800

    add: util filter function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,7 @@
 export function formatPrice(p: number) {
 	return p.toLocaleString('en-US', { style: 'currency', currency: 'PHP' })
 }
+
+export function filterFalsy<T>(item?: T | null): item is T {
+	return !!item
+}
